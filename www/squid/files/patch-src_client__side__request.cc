f1fb6509f3e371b23115c848441fe69ad57f839d
davidp <david@netdiverse.com>
2019-12-31 15:49:58 -0700

    Allow failed host verify if sslBump == peek

--- www/squid/files/patch-src_client__side__request.cc
+++ www/squid/files/patch-src_client__side__request.cc
@@ -21,15 +21,17 @@ Date:   2018-01-21 08:07:08 +1300
      }
      request->x_forwarded_for_iterator.clean();
      request->flags.done_follow_x_forwarded_for = true;
-@@ -561,7 +561,6 @@
-     debugs(85, DBG_IMPORTANT, "SECURITY ALERT: on URL: " << urlCanonical(http->request));
- 
-     // IP address validation for Host: failed. reject the connection.
--    http->getConn()->quitAfterError(http->request);
-     clientStreamNode *node = (clientStreamNode *)http->client_stream.tail->prev->data;
-     clientReplyContext *repContext = dynamic_cast<clientReplyContext *>(node->data.getRaw());
-     assert (repContext);
-@@ -1419,6 +1418,11 @@
+@@ -540,7 +540,8 @@
+ {
+     // IP address validation for Host: failed. Admin wants to ignore them.
+     // NP: we do not yet handle CONNECT tunnels well, so ignore for them
+-    if (!Config.onoff.hostStrictVerify && http->request->method != Http::METHOD_CONNECT) {
++    const Ssl::BumpMode bumpMode = http->getConn()->sslBumpMode;
++    if ((!Config.onoff.hostStrictVerify && http->request->method != Http::METHOD_CONNECT) || (!Config.onoff.hostStrictVerify && bumpMode == "peek")) {
+         debugs(85, 3, "SECURITY ALERT: Host header forgery detected on " << http->getConn()->clientConnection <<
+                " (" << A << " does not match " << B << ") on URL: " << urlCanonical(http->request));
+
+@@ -1419,6 +1420,11 @@
  bool
  ClientRequestContext::sslBumpAccessCheck()
  {
